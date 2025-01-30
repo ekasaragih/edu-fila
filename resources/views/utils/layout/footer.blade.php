378fcc7f64cd1e929c5146326734b8a04ee9889f
ekasaragih <saragiheka27@gmail.com>
2025-01-30 11:11:42 +0700

    update footer to add watermark

--- resources/views/utils/layout/footer.blade.php
+++ resources/views/utils/layout/footer.blade.php
@@ -1,4 +1,18 @@
-<!-- Footer Section -->
-<footer class="bg-dark text-white text-center py-4">
-    <p>&copy; 2025 Edu Fila. All Rights Reserved.</p>
+<footer class="footer pt-3 text-center">
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+                <div class="copyright text-center text-sm text-muted text-lg-start">
+                    ©
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script> Edu Fila. All Rights Reserved,
+                    made with <i class="fa fa-heart"></i> by
+                    <a href="#" class="font-weight-bold" target="_blank">UUU Team
+                    </a>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
